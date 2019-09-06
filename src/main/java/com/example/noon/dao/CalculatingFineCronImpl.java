c045040496036bbdccd130516879a8819fbd4a55
Rohit Saraf <rohitsaraf@197nodnb24699.one97.delhi.net>
2019-09-06 18:40:43 +0530

    Committing code for Calculating fine cron impl

--- src/main/java/com/example/noon/dao/CalculatingFineCronImpl.java
+++ src/main/java/com/example/noon/dao/CalculatingFineCronImpl.java
@@ -1,16 +1,20 @@
 package com.example.noon.dao;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.example.noon.entity.FineDetailInfo;
+import com.example.noon.entity.User;
 import com.example.noon.entity.UserBookDetail;
 
 @Component("calculatingFineCronImpl")
 public class CalculatingFineCronImpl {
 
-	
 	@Scheduled(cron = "0 0 12 * * *")
 	public void calculateFineCron() {
 		System.out.println("Inside calculating fine through cron....");
@@ -19,8 +23,20 @@ public class CalculatingFineCronImpl {
 
 	private void calculateFineCronUtil() {
 		List<UserBookDetail> userBookList = NoonServiceDaoImpl.allUserBookDetails;
+		LinkedHashMap<User, LinkedList<FineDetailInfo>> userFinedetailsMap = NoonServiceDaoImpl.userFineDetails;
 		if (userBookList != null && userBookList.size() > 0) {
-			
+			for (UserBookDetail userBookDetail : userBookList) {
+				Date tentativeReturnDate = userBookDetail.getTentativeReturnDate();
+				Date currentDate = new Date();
+				if (currentDate.compareTo(tentativeReturnDate) > 0) {
+					LinkedList<FineDetailInfo> userFineList = userFinedetailsMap.get(userBookDetail.getUser());
+					if (userFineList == null) {
+						
+					} else {
+
+					}
+				}
+			}
 		}
 	}
 }
